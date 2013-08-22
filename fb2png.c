7653fcb997eadc34343ad83cbcb225a00c184360
AndrewFromMelbourne <arduncan@bigpond.com>
2013-08-22 22:26:43 +1000

    Fixed bug with xoffset and yoffset

--- fb2png.c
+++ fb2png.c
@@ -211,8 +211,8 @@ int main(int argc, char *argv[])
         {
             int pb_offset = 3 * x;
 
-            size_t fb_offset = (x + vinfo.xoffset) * (bytes_per_pixel)
-                             + (y + vinfo.yoffset) * finfo.line_length;
+            size_t fb_offset = x * (bytes_per_pixel)
+                             + y * finfo.line_length;
 
             uint32_t pixel = 0;
 
